aa17da65468613efc4a173dbbf927710316e2c28
Kyle White <kylewhite@gmail.com>
2019-08-09 10:39:36 -0400

    Try test with mod vendor

--- etc/testing/pfs_server.sh
+++ etc/testing/pfs_server.sh
@@ -1,3 +1,3 @@
 # grep out PFS server logs, as otherwise the test output is too verbose to
 # follow and breaks travis
-go test -v ./src/server/pfs/server -timeout 3600s | grep -v "$(date +^%FT)"
+go test  -mod=vendor -v ./src/server/pfs/server -timeout 3600s | grep -v "$(date +^%FT)"
